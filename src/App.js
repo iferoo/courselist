02261835932509b7600f61cfe0baecc518361bb2
Eyad Mohamed <eyadmohamed2859@gmail.com>
2022-04-04 01:10:44 +0200

    init Course App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CourseForm from "./components/CourseForm/CourseForm";
+import CourseList from "./components/CourseList/CourseList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    courses: [
+      { name: "HTML" },
+      { name: "CSS" },
+      { name: "jQuery" },
+      { name: "C++" },
+    ],
+    current: "",
+    messegeError: false,
+  };
+
+  updateCourse = (e) => {
+    this.setState({
+      current: e.target.value,
+    });
+  };
+
+  addCourse = (e) => {
+    e.preventDefault();
+    let { courses, current } = this.state;
+    current === ""
+      ? this.setState({ messegeError: !this.state.messegeError })
+      : courses.push({ name: current });
+
+    this.setState({ courses, current: "" });
+  };
+
+  deleteCourse = (index) => {
+    let courses = this.state.courses;
+    courses.splice(index, 1);
+    this.setState({ courses });
+  };
+
+  editCourse = (index, value) => {
+    let { courses } = this.state;
+    let course = courses[index];
+    course["name"] = value;
+    this.setState({
+      courses,
+    });
+  };
+  render() {
+    const { courses } = this.state;
+    const courseList = courses.map((course, index) => {
+      return (
+        <CourseList
+          details={course}
+          key={index}
+          index={index}
+          deleteCourse={this.deleteCourse}
+          editCourse={this.editCourse}
+        />
+      );
+    });
+    return (
+      <section className="App">
+        <h2>Add Courses</h2>
+        {this.state.messegeError === false ? (
+          <CourseForm
+            updateCourse={this.updateCourse}
+            addCourse={this.addCourse}
+            current={this.state.current}
+          />
+        ) : (
+          <div><p>There is no value</p>
+          <button onClick={() => this.setState({ messegeError: !this.state.messegeError })}>&times;</button>
+          </div>
+        )}
+
+        <ul>
+          {this.state.courses.length === 0 ? (
+            <p>There is no Course</p>
+          ) : (
+            courseList
+          )}
+        </ul>
+      </section>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/CourseList/CourseList.js
@@ -0,0 +1,67 @@
+import React, { Component, Fragment } from "react";
+
+class CourseList extends Component {
+  state = {
+    isEdit: false,
+  };
+  renderCourse = () => {
+    return (
+      <li>
+        <span>{this.props.details.name}</span>
+        <button
+          onClick={() => {
+            this.toggleState();
+          }}
+        >
+          Edit Course
+        </button>
+        <button
+          onClick={() => {
+            this.props.deleteCourse(this.props.index);
+          }}
+        >
+          Delete Course
+        </button>
+      </li>
+    );
+  };
+
+  renderUpdateForm = () => {
+    return (
+      <form onSubmit={this.updateCourseItem}>
+        <input
+          type="text"
+          ref={(v) => {
+            this.input = v;
+          }}
+          defaultValue={this.props.details.name}
+        ></input>
+        <button>Update Course</button>
+      </form>
+    );
+  };
+
+  updateCourseItem = (e) => {
+    e.preventDefault();
+    this.props.editCourse(this.props.index, this.input.value);
+    this.toggleState();
+  };
+
+  //toggleState
+  toggleState = () => {
+    let { isEdit } = this.state;
+    this.setState({
+      isEdit: !isEdit,
+    });
+  };
+  render() {
+    let { isEdit } = this.state;
+    return (
+      <Fragment>
+        {isEdit ? this.renderUpdateForm() : this.renderCourse()}
+      </Fragment>
+    );
+  }
+}
+
+export default CourseList;
